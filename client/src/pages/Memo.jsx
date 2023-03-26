12b6b5bf71a93e8e92b81365ca185d91f6a5c463
sakipiyo <chyole4444bakaparm@msn.com>
2023-03-27 02:39:56 +0900

    Create getOneApi and Memo component

--- client/src/pages/Memo.jsx
+++ client/src/pages/Memo.jsx
@@ -1,9 +1,43 @@
+import { Box, IconButton, TextField } from '@mui/material';
 import React from 'react'
+import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 
 const Memo = () => {
   return (
     <div>
-      Memo
+      <>
+        <Box 
+            sx={{ 
+                display: "flex", 
+                alignItems: "center",
+                justifyContent: "space-between",
+                width: "100%",
+            }}>
+                <IconButton>
+                    <StarBorderOutlinedIcon />
+                </IconButton>
+                <IconButton 
+                    variant="outlined"
+                    color="error"
+                >
+                    <DeleteOutlinedIcon />
+                </IconButton>
+            </Box>
+            <Box 
+                sx={{
+                    padding: "10px 50px"
+                }}>
+                    <TextField 
+                        placeholder="無題"
+                        variant="outlined"
+                        fullWidth />
+                    <TextField 
+                        placeholder="追加"
+                        variant="outlined"
+                        fullWidth />
+            </Box>
+      </>
     </div>
   )
 }
